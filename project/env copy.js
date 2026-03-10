9b45c8302f8612322f83e353f2b3f99269fe2acb
Navneet Varshney <navneetva252@gmail.com>
2026-03-10 14:58:14 +0530

    feat(env): add API_BASE_URL and set AUTH_MODE to EMAIL, remove duplicate env copy

--- project/env copy.js
+++
@@ -1,9 +0,0 @@
-window.RUNTIME_ENV = {
-  AUTH_MODE: "EITHER"
-  //OPTION :
-  // 1. PHONE
-  // 2. EMAIL
-  // 3. EITHER
-  // 4. BOTH
-};
-
--- project/env.js
+++ project/env.js
@@ -1,9 +1,11 @@
 window.RUNTIME_ENV = {
-  AUTH_MODE: "EITHER"
+  AUTH_MODE: "EMAIL",
   //OPTION :
   // 1. PHONE
   // 2. EMAIL
   // 3. EITHER
   // 4. BOTH
+
+  API_BASE_URL: "http://localhost:8080/custom-auth-service/api/v1",
 };
 
