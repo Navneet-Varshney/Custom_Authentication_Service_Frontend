7370237dc6cc1dc446136d2f3e2d5ec1d00bef66
Navneet Varshney <navneetva252@gmail.com>
2026-03-30 20:07:05 +0530

    feat: Add product-vision service with API methods

---
+++ srems-frontend/src/js/services/product-vision.service.js
@@ -0,0 +1,77 @@
+/**
+ * product-vision.service.js
+ * Product vision management operations
+ */
+
+import apiClient from './api.js';
+import { API_CONFIG } from '../utils/constants.js';
+
+class ProductVisionService {
+  /**
+   * Create product vision
+   */
+  async createProductVision(visionData) {
+    return apiClient.post(
+      `${API_CONFIG.ENDPOINTS.PRODUCT_VISION}/create`,
+      visionData
+    );
+  }
+
+  /**
+   * Get all product visions
+   */
+  async getProductVisions(page = 1, pageSize = 10) {
+    return apiClient.get(
+      `${API_CONFIG.ENDPOINTS.PRODUCT_VISION}/list?page=${page}&pageSize=${pageSize}`
+    );
+  }
+
+  /**
+   * Get product vision by ID
+   */
+  async getProductVisionById(visionId) {
+    return apiClient.get(
+      `${API_CONFIG.ENDPOINTS.PRODUCT_VISION}/get/${visionId}`
+    );
+  }
+
+  /**
+   * Update product vision
+   */
+  async updateProductVision(visionId, updateData) {
+    return apiClient.patch(
+      `${API_CONFIG.ENDPOINTS.PRODUCT_VISION}/update/${visionId}`,
+      updateData
+    );
+  }
+
+  /**
+   * Delete product vision
+   */
+  async deleteProductVision(visionId) {
+    return apiClient.delete(
+      `${API_CONFIG.ENDPOINTS.PRODUCT_VISION}/delete/${visionId}`
+    );
+  }
+
+  /**
+   * Get product visions by product
+   */
+  async getProductVisionsByProduct(productId) {
+    return apiClient.get(
+      `${API_CONFIG.ENDPOINTS.PRODUCT_VISION}/product/${productId}`
+    );
+  }
+
+  /**
+   * Get product visions by status
+   */
+  async getProductVisionsByStatus(status) {
+    return apiClient.get(
+      `${API_CONFIG.ENDPOINTS.PRODUCT_VISION}/status/${status}`
+    );
+  }
+}
+
+export const productVisionService = new ProductVisionService();
+export default productVisionService;
